2579ee8cf4f0234200549f67a3c8296623d60b1e
Abrahan <scr44gr@gmail.com>
2022-03-24 19:10:16 -0400

    add: skip_whitespace, letter and numbers identifiers

--- lau/lexer.py
+++ lau/lexer.py
@@ -12,10 +12,33 @@ class Lexer:
         self._read_character()
     
     def next_token(self) -> Token:
-        token_type = Tokens.exists(self._character)
-        token = Token(token_type, self._character)
+        self._skip_whitespace()
+
+        if Tokens.lookup(self._character):
+            token_type = Tokens.lookup_token_type(self._character)
+            token = Token(token_type, self._character)
+
+        elif self._is_letter(self._character):
+            literal = self._read_identifier()
+            token_type = Tokens.lookup_token_type(literal)
+            return Token(token_type, literal)
+
+        elif self._is_number(self._character):
+            literal = self._read_number()
+            return Token(TokenType.INT, literal)
+
+        else:
+            token = Token(TokenType.ILLEGAL, self._character)
+
         self._read_character()
         return token
+
+    def _is_letter(self, value: str) -> bool:
+
+        return bool(match(Tokens.LETTER, value))
+    
+    def _is_number(self, value: str) -> bool:
+        return bool(match(Tokens.NUMBER, value))
 
     def _read_character(self) -> None:
         
@@ -26,3 +49,21 @@ class Lexer:
         
         self._position = self._read_position
         self._read_position += 1
+
+    def _read_identifier(self) -> str:
+        initial_position = self._position
+        while self._is_letter(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+    
+    def _read_number(self) -> str:
+        initial_position = self._position
+        while self._is_number(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+
+    def _skip_whitespace(self) -> None:
+        while match(Tokens.WHITESPACE, self._character):
+            self._read_character()
