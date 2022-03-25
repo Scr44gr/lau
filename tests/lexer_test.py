ec89ca332875a467eba910e0b09ab742084bcc83
Abrahan <scr44gr@gmail.com>
2022-03-24 20:00:13 -0400

    add: test_function_declaration and call

--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -10,7 +10,7 @@ class LexerTest(TestCase):
         lexer: Lexer = Lexer(source)
 
         tokens: List[Token] = []
-        for i in range(len(source)):
+        for _ in range(len(source)):
             tokens.append(lexer.next_token())
         
         expected_tokens: List[Token] = [
@@ -26,7 +26,7 @@ class LexerTest(TestCase):
         lexer: Lexer = Lexer(source)
 
         tokens: List[Token] = []
-        for i in range(len(source)):
+        for _ in range(len(source)):
             tokens.append(lexer.next_token())
         
         expected_tokens: List[Token] = [
@@ -52,10 +52,10 @@ class LexerTest(TestCase):
         self.assertEqual(tokens, expected_tokens)
     
     def test_delimiters(self):
-        source = '{}(),;'
-        lexer = Lexer(source)
+        source: str = '{}(),;'
+        lexer: Lexer = Lexer(source)
 
-        tokens = []
+        tokens: List[Token] = []
         for _ in range(len(source)):
             tokens.append(lexer.next_token())
         
@@ -70,10 +70,10 @@ class LexerTest(TestCase):
         self.assertEqual(tokens, expected_tokens)
     
     def test_identifier(self):
-        source = 'let number = 5;'
-        lexer = Lexer(source)
+        source: str = 'let number = 5;'
+        lexer: Lexer = Lexer(source)
 
-        tokens = []
+        tokens: List[Token] = []
         for _ in range(5):
             tokens.append(lexer.next_token())
 
@@ -82,6 +82,49 @@ class LexerTest(TestCase):
             Token(TokenType.IDENT, 'number'),
             Token(TokenType.ASSIGN, '='),
             Token(TokenType.INT, '5'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+        self.assertEqual(tokens, expected_tokens)
+    
+    def test_function_declaration(self):
+        source: str = 'fn add(x, y) { x + y }'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for _ in range(12):
+            tokens.append(lexer.next_token())
+        
+        expected_tokens = [
+            Token(TokenType.FUNCTION, 'fn'),
+            Token(TokenType.IDENT, 'add'),
+            Token(TokenType.LPARENT, '('),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.RPARENT, ')'),
+            Token(TokenType.LBRACE, '{'),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.PLUS, '+'),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.RBRACE, '}'),
+        ]
+        self.assertEqual(tokens, expected_tokens)
+    
+    def test_function_call(self):
+        source: str = 'add(5, 10);'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for _ in range(7):
+            tokens.append(lexer.next_token())
+        
+        expected_tokens = [
+            Token(TokenType.IDENT, 'add'),
+            Token(TokenType.LPARENT, '('),
+            Token(TokenType.INT, '5'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.INT, '10'),
+            Token(TokenType.RPARENT, ')'),
             Token(TokenType.SEMICOLON, ';'),
         ]
         self.assertEqual(tokens, expected_tokens)
